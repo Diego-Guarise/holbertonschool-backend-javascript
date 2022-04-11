da8b3915c502e0bc34a051fe710e9e82a211cf32
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-11 16:06:40 +0000

    task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
-export default function getNeighborhoodsList() { 
+export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeghborhood = add = (newNeighborhood) => { return self.sanFranciscoNeighborhoods.push(newNeighborhood);
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
-
