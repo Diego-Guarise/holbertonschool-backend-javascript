046bd6da08ae8dd1c72568d6f921f5853c1c8c83
root <root@H64.hbtn.io>
2022-04-13 11:32:25 -0300

    task 3 eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,12 +4,12 @@ export default async function handleProfileSignup() {
   const first = await createUser();
   const last = await createUser();
   const photo = await uploadPhoto();
-//  console.log(first.firstName);
-//  console.log(last);
-//  console.log(photo);
+  //  console.log(first.firstName);
+  //  console.log(last);
+  //  console.log(photo);
   return new Promise(() => {
     if (first.firstName && last.lastName && photo.body) {
       console.log(photo.body, first.firstName, last.lastName);
-    } else { console.log('Signup system offline') }
+    } else { console.log('Signup system offline'); }
   });
 }
