35f87718b9d35d4ba556e58d2258ded5ccc10aeb
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-13 13:36:56 +0000

    task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+/* Return a Promise using this prototype function getResponseFromAPI() */
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success'})
+    } else {
+      reject(
+        Error('The fake API is not working currently')
+        )
+    }
+  });
+}
+
