33fa1c90b747dfcbbca449b95e8cd00b7b22dfad
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-13 14:10:11 -0300

    task 4 es lint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  const first = await createUser();
-  const last = await createUser();
-  const photo = await uploadPhoto();
-  //  console.log(first.firstName);
-  //  console.log(last);
-  //  console.log(photo);
-  return new Promise(() => {
-    if (first.firstName && last.lastName && photo.body) {
-      console.log(photo.body, first.firstName, last.lastName);
-    } else { console.log('Signup system offline'); }
-  });
+export default function handleProfileSignup() {
+  const first = createUser();
+  const last = createUser();
+  const photo = uploadPhoto();
+  
+  console.log(Promise.resolve.first);
 }
--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,4 @@
 /* returns a resolved promise with this object */
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve( {firstName, lastName} );
+  return Promise.resolve({ firstName, lastName });
 }
