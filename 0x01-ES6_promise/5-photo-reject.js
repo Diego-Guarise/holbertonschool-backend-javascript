61279a71f241de815a942f01eeb13554fb6a923f
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-13 14:25:52 -0300

    task 5 eslint

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-      const hola = reject(Error(`${filename} cannot be processed`));
-  });
+  return Promise.reject(Error(`${filename} cannot be processed`));
 }
