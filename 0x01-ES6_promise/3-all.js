4c35e7178d616dbadf3462ee9522f476251badc5
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-13 13:37:18 +0000

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function handleProfileSignup() {
+  const first = await createUser();
+  const last = await createUser();
+  const photo = await uploadPhoto();
+//  console.log(first.firstName);
+//  console.log(last);
+//  console.log(photo);
+  return new Promise(() => {
+    if (first.firstName && last.lastName && photo.body) {
+      console.log(photo.body, first.firstName, last.lastName);
+    } else { console.log('Signup system offline') }
+  });
+}
