93a9220219d69b6080e97ba515d0a82f0a4893e6
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-13 23:29:36 -0300

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,25 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const user = signUpUser(firstName, lastName);
-    const photo = uploadPhoto(fileName);
-    return Promise.all([user, photo]);
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((res) => {
+    const array = [];
+    res.forEach((elem) => {
+      if (elem.status === 'fulfilled') {
+        array.push({
+          status: elem.status,
+          value: elem.value,
+        });
+      } else {
+        array.push({
+          status: elem.status,
+          value: `${elem.reason}`,
+        });
+      }
+    });
+    return array;
+  });
 }
