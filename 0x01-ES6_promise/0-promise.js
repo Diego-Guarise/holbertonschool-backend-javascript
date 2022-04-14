0e06f778301a17bf5923d034aa676ae25ee14959
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-14 14:36:15 -0300

    deleted

--- 0x01-ES6_promise/0-promise.js
+++
@@ -1,5 +0,0 @@
-/* Return a Promise using this prototype function getResponseFromAPI() */
-export default function getResponseFromAPI() {
-  return new Promise(() => {
-  });
-}
--- 0x01-ES6_promise/4-user-promise.js
+++
@@ -1,4 +0,0 @@
-/* returns a resolved promise with this object */
-export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({ firstName, lastName });
-}
