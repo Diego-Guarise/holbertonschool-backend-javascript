a66366d00fa9cdd97ddba3fcbee8b370945a65e3
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-14 16:00:13 -0300

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,13 @@
 export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
     console.log('Got a response from the API');
     if (Promise.resolve(promise)) {
-        return {
-            status: 200,
-            body: 'success',
-        };
-    } else {
-        return Error()
+      return {
+        status: 200,
+        body: 'success',
+      };
     }
+    console.log('Got a response from the API');
+    return Error();
+  });
 }
