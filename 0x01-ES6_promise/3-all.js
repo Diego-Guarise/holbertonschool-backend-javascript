d50ec011cba38ee53b77aa4e5e7a9772718b599f
Diego-Guarise <l.guariselopez@gmail.com>
2022-04-13 23:50:56 -0300

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const first = createUser();
-  const last = createUser();
   const photo = uploadPhoto();
-  
-  console.log(Promise.resolve.first);
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
